67da8e772b8882d1006983c51f14bb0327483b46
Dedan Irungu <dedanirungu@gmail.com>
2023-07-26 13:58:37 +0300

    adding listtable and formbuilder fuction to all model so that data can be rendered via json

--- Entities/Check.php
+++ Entities/Check.php
@@ -5,6 +5,9 @@ namespace Modules\Expense\Entities;
 use Modules\Base\Entities\BaseModel;
 use Illuminate\Database\Schema\Blueprint;
 
+use Modules\Core\Classes\Views\ListTable;
+use Modules\Core\Classes\Views\FormBuilder;
+
 class Check extends BaseModel
 {
 
@@ -12,6 +15,56 @@ class Check extends BaseModel
     public $migrationDependancy = [];
     protected $table = "expense_check";
 
+
+    public function listTable(){
+        // listing view fields
+        $fields = new ListTable();
+
+        $fields->name('trn_no')->type('text')->ordering(true);
+        $fields->name('check_no')->type('text')->ordering(true);
+        $fields->name('voucher_type')->type('text')->ordering(true);
+        $fields->name('amount')->type('text')->ordering(true);
+        $fields->name('bank')->type('text')->ordering(true);
+        $fields->name('name')->type('text')->ordering(true);
+        $fields->name('pay_to')->type('text')->ordering(true);
+
+
+        return $fields;
+
+    }
+    
+    public function formBuilder(){
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('trn_no')->type('text')->group('w-1/2');
+        $fields->name('check_no')->type('text')->group('w-1/2');
+        $fields->name('voucher_type')->type('text')->group('w-1/2');
+        $fields->name('amount')->type('text')->group('w-1/2');
+        $fields->name('bank')->type('text')->group('w-1/2');
+        $fields->name('name')->type('text')->group('w-1/2');
+        $fields->name('pay_to')->type('text')->group('w-1/2');
+
+        return $fields;
+
+    }
+
+    public function filter(){
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('trn_no')->type('text')->group('w-1/6');
+        $fields->name('check_no')->type('text')->group('w-1/6');
+        $fields->name('voucher_type')->type('text')->group('w-1/6');
+        $fields->name('amount')->type('text')->group('w-1/6');
+        $fields->name('bank')->type('text')->group('w-1/6');
+        $fields->name('name')->type('text')->group('w-1/6');
+        $fields->name('pay_to')->type('text')->group('w-1/6');
+
+
+        return $fields;
+
+    }
     /**
      * List of fields for managing postings.
      *
--- Entities/Detail.php
+++ Entities/Detail.php
@@ -5,6 +5,9 @@ namespace Modules\Expense\Entities;
 use Modules\Base\Entities\BaseModel;
 use Illuminate\Database\Schema\Blueprint;
 
+use Modules\Core\Classes\Views\ListTable;
+use Modules\Core\Classes\Views\FormBuilder;
+
 class Detail extends BaseModel
 {
 
@@ -12,6 +15,46 @@ class Detail extends BaseModel
     public $migrationDependancy = [];
     protected $table = "expense_detail";
 
+
+    public function listTable(){
+        // listing view fields
+        $fields = new ListTable();
+
+        $fields->name('trn_no')->type('text')->ordering(true);
+        $fields->name('ledger_id')->type('recordpicker')->table('account_ledger')->ordering(true);
+        $fields->name('particulars')->type('text')->ordering(true);
+        $fields->name('amount')->type('text')->ordering(true);
+
+
+        return $fields;
+
+    }
+    
+    public function formBuilder(){
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('trn_no')->type('text')->group('w-1/2');
+        $fields->name('ledger_id')->type('recordpicker')->table('account_ledger')->group('w-1/2');
+        $fields->name('particulars')->type('text')->group('w-1/2');
+        $fields->name('amount')->type('text')->group('w-1/2');
+
+
+        return $fields;
+
+    }
+
+    public function filter(){
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('trn_no')->type('text')->group('w-1/6');
+        $fields->name('ledger_id')->type('recordpicker')->table('account_ledger')->group('w-1/6');
+        $fields->name('amount')->type('text')->group('w-1/6');
+
+        return $fields;
+
+    }
     /**
      * List of fields for managing postings.
      *
