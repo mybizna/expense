7b7f7af7401fd1d09f1a17d5c886f4d69341c5ef
Dedan <dedanirungu@gmail.com>
2023-11-14 19:23:30 +0300

    Changes to expenses table

--- Entities/Expense.php
+++ Entities/Expense.php
@@ -3,6 +3,7 @@
 namespace Modules\Expense\Entities;
 
 use Illuminate\Database\Schema\Blueprint;
+use Modules\Base\Classes\Migration;
 use Modules\Base\Entities\BaseModel;
 
 class Expense extends BaseModel
@@ -13,9 +14,8 @@ class Expense extends BaseModel
      * @var array<string>
      */
     protected $fillable = [
-        'voucher_no', 'people_id', 'people_name', 'address', 'trn_date', 'amount',
-        'ref', 'check_no', 'particulars', 'status', 'trn_by', 'transaction_charge',
-        'trn_by_ledger_id', 'attachments',
+        'title', 'expense_no', 'partner_id', 'due_date', 'module', 'model', 'status',
+        'description', 'is_posted', 'total',
     ];
 
     /**
@@ -23,21 +23,28 @@ class Expense extends BaseModel
      *
      * @var array<string>
      */
-    public $rec_names = ['voucher_no'];
+    public $rec_names = ['title'];
 
     /**
      * List of tables names that are need in this model during migration.
      *
      * @var array<string>
      */
-    public array $migrationDependancy = [];
+    public array $migrationDependancy = ['partner'];
 
     /**
-     * The fields that can be filled
+     * The table associated with the model.
      *
      * @var string
      */
     protected $table = "expense";
+
+    /**
+     * Set if model is visible from frontend.
+     *
+     * @var bool
+     */
+    protected bool $show_frontend = true;
 
     /**
      * List of fields to be migrated to the datebase when creating or updating model during migration.
@@ -49,21 +56,20 @@ class Expense extends BaseModel
     {
         $this->fields = $table ?? new Blueprint($this->table);
 
+        $statuses = ['draft' => 'Draft', 'pending' => 'Pending', 'partial' => 'Partial', 'paid' => 'Paid', 'closed' => 'Closed', 'void' => 'Void'];
+        $statuses_color = ['draft' => 'gray', 'pending' => 'sky', 'partial' => 'indigo', 'paid' => 'green', 'closed' => 'orange', 'void' => 'red'];
+
         $this->fields->increments('id')->html('hidden');
-        $this->fields->integer('voucher_no')->nullable()->html('text');
-        $this->fields->integer('people_id')->nullable()->html('text');
-        $this->fields->string('people_name')->nullable()->html('text');
-        $this->fields->string('address')->nullable()->html('text');
-        $this->fields->date('trn_date')->nullable()->html('date');
-        $this->fields->decimal('amount', 20, 2)->default(0.00)->html('amount');
-        $this->fields->string('ref')->nullable()->html('textarea');
-        $this->fields->string('check_no')->nullable()->html('text');
-        $this->fields->string('particulars')->nullable()->html('textarea');
-        $this->fields->integer('status')->nullable()->html('text');
-        $this->fields->integer('trn_by')->nullable()->html('text');
-        $this->fields->decimal('transaction_charge', 20, 2)->default(0.00)->html('amount');
-        $this->fields->integer('trn_by_ledger_id')->nullable()->html('text');
-        $this->fields->string('attachments')->nullable()->html('files');
+        $this->fields->string('title')->html('text');
+        $this->fields->char('expense_no', 100)->html('text');
+        $this->fields->foreignId('partner_id')->html('recordpicker')->relation(['partner']);
+        $this->fields->date('due_date')->useCurrent()->html('datetime');
+        $this->fields->string('module')->default('Account')->html('text');
+        $this->fields->string('model')->default('Expense')->html('text');
+        $this->fields->enum('status', array_keys($statuses))->html('switch')->default('draft')->options($statuses)->color($statuses_color)->nullable();
+        $this->fields->string('description')->nullable()->html('textarea');
+        $this->fields->tinyInteger('is_posted')->default(0)->html('switch');
+        $this->fields->decimal('total', 20, 2)->nullable()->html('amount');
     }
 
     /**
@@ -71,9 +77,15 @@ class Expense extends BaseModel
      */
     public function structure($structure): array
     {
-        $structure['table'] = ['voucher_no', 'people_id', 'trn_date', 'amount', 'status', 'trn_by', 'transaction_charge', 'trn_by_ledger_id'];
-        $structure['filter'] = ['voucher_no', 'people_id', 'trn_date', 'status', 'trn_by', 'trn_by_ledger_id'];
+        $structure['table'] = ['title', 'expense_no', 'partner_id', 'due_date', 'status', 'is_posted', 'total'];
+        $structure['form'] = [
+            ['label' => 'Title', 'class' => 'col-span-full', 'fields' => ['title']],
+            ['label' => 'Expense', 'class' => 'col-span-6', 'fields' => ['expense_no', 'partner_id', 'due_date']],
+            ['label' => 'Setting', 'class' => 'col-span-6', 'fields' => ['status', 'is_posted', 'total']],
+        ];
+        $structure['filter'] = ['title', 'expense_no', 'partner_id', 'due_date', 'status'];
 
         return $structure;
     }
+
 }
---
+++ Entities/Item.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Modules\Expense\Entities;
+
+use Illuminate\Database\Schema\Blueprint;
+use Modules\Base\Classes\Migration;
+use Modules\Base\Entities\BaseModel;
+
+class Item extends BaseModel
+{
+    /**
+     * The fields that can be filled
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'title', 'expense_id', 'ledger_id', 'price', 'amount', 'quantity',
+        'module', 'model', 'item_id',
+    ];
+
+    /**
+     * The fields that are to be render when performing relationship queries.
+     *
+     * @var array<string>
+     */
+    public $rec_names = ['title'];
+
+    /**
+     * List of tables names that are need in this model during migration.
+     *
+     * @var array<string>
+     */
+    public array $migrationDependancy = ['expense', 'account_ledger'];
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "expense_item";
+
+    /**
+     * List of fields to be migrated to the datebase when creating or updating model during migration.
+     *
+     * @param Blueprint $table
+     * @return void
+     */
+    public function fields(Blueprint $table = null): void
+    {
+
+        $this->fields = $table ?? new Blueprint($this->table);
+
+        $this->fields->increments('id')->html('hidden');
+        $this->fields->string('title')->html('text');
+        $this->fields->foreignId('expense_id')->html('recordpicker')->relation(['expense', 'expense']);
+        $this->fields->foreignId('ledger_id')->html('recordpicker')->relation(['account', 'ledger']);
+        $this->fields->decimal('price', 20, 2)->default(0.00)->html('amount');
+        $this->fields->decimal('amount', 20, 2)->default(0.00)->html('amount');
+        $this->fields->string('module')->nullable()->html('text');
+        $this->fields->string('model')->nullable()->html('text');
+        $this->fields->foreignId('item_id')->nullable()->html('text');
+        $this->fields->integer('quantity')->nullable()->html('number');
+    }
+
+    /**
+     * List of structure for this model.
+     */
+    public function structure($structure): array
+    {
+        $structure['table'] = ['title', 'expense_id', 'ledger_id', 'price', 'amount', 'quantity'];
+        $structure['form'] = [
+            ['label' => 'Title', 'class' => 'col-span-full', 'fields' => ['title']],
+            ['label' => 'Expense Item', 'class' => 'col-span-6', 'fields' => ['expense_id', 'ledger_id', 'quantity']],
+            ['label' => 'Amount', 'class' => 'col-span-6', 'fields' => ['price', 'amount']],
+        ];
+        $structure['filter'] = ['title', 'expense_id', 'ledger_id'];
+
+        return $structure;
+    }
+
+}
