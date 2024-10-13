2632a65413efc7234a9e47f0f12f0293955715c3
Dedan <dedanirungu@gmail.com>
2024-10-13 16:28:36 +0300

    Release 2024.14

---
+++ app/Filament/Resources/ExpenseResource/Pages/Creating.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Expense\Filament\Resources\ExpenseResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\CreatingBase;
+
+// Class List that extends ListBase
+class Creating extends CreatingBase
+{
+}
---
+++ app/Filament/Resources/ExpenseResource/Pages/Editing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Expense\Filament\Resources\ExpenseResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\EditingBase;
+
+// Class List that extends ListBase
+class Editing extends EditingBase
+{
+}
